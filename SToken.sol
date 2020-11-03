0d1628ae3ce3815acb3a4968f92b0e0c960055cd
cryptoblockfarmer <cryptoblocksugar@protonmail.com>
2020-11-04 00:59:46 +0100

    timelock address updated

--- SToken.sol
+++ SToken.sol
@@ -15,7 +15,7 @@ pragma solidity 0.6.12;
 
  
 // CakeToken with Governance.
-contract SToken is BEP20('YFCGold', 'YFCG') {
+contract SToken is BEP20('YFCSilver', 'YFCS') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
